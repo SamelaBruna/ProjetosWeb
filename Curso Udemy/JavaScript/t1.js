4b9a69e8af36aab0b05a3f24865aac081bfea235
Samela Bruna Ferreira <samelabrunaferreira@hotmail.com>
2024-06-08 12:18:46 -0400

    Adicionando pasta udemy e atualizando countdown

---
+++ Curso Udemy/JavaScript/t1.js
@@ -0,0 +1,18 @@
+/* console.log(
+  'Meu nome é "BRUNA". Estou aprendendo JavaScript às',
+  10,
+  "da manhã!"
+); */
+
+let nome = "fulano";
+let idade;
+console.log(nome, "nasceu em 1997");
+console.log(idade);
+idade = 35;
+console.log(idade);
+const sobrenome = "sicrano";
+console.log(nome, sobrenome);
+console.log(typeof idade);
+let num1 = 10;
+let num2 = Number("2");
+console.log(num1 + num2);
--- RocketSeat Discover/Desafios/Countdown/script.js
+++ RocketSeat Discover/Desafios/Countdown/script.js
@@ -1,4 +1,4 @@
-const countdownHour = new Date("September 23, 2023 00:00:00").getTime();
+const countdownHour = new Date("December 06, 2024 00:00:00").getTime();
 let day = document.getElementById("days");
 let hour = document.getElementById("hours");
 let minute = document.getElementById("minutes");
