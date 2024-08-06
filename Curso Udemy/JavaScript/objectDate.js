f1ebc30180f3e9ec41d53714fbc94c642b4572ae
Samela Bruna Ferreira <samelabrunaferreira@hotmail.com>
2024-08-06 11:25:55 -0400

    object date

---
+++ Curso Udemy/JavaScript/objectDate.js
@@ -0,0 +1,26 @@
+function zeroToTheLeft(num) {
+  return num >= 10 ? num : `0${num}`;
+}
+
+// function formatDate(date) {
+//   const day = zeroToTheLeft(date.getDate());
+//   const month = zeroToTheLeft(date.getMonth());
+//   const year = zeroToTheLeft(date.getFullYear());
+//   const hour = zeroToTheLeft(date.getHours());
+//   const minute = zeroToTheLeft(date.getMinutes());
+//   const seconds = zeroToTheLeft(date.getSeconds());
+//   return `${day}/${month}/${year} ${hour}:${minute}:${seconds}`;
+// }
+
+// const date = new Date();
+// const brazilDate = formatDate(date);
+// console.log(brazilDate);
+
+//OUTRA FORMA:
+
+const date = new Date();
+const brazilDate =
+  date.toLocaleDateString("pt-br") +
+  " " +
+  date.toLocaleTimeString("pt-br", { hour: "2-digit", minute: "2-digit" });
+console.log(brazilDate);
